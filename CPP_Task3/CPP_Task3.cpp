0dc1a4c258aaa9960efa33e1c1b3d3fda9307f3c
iIlyaM <imoldavskiy@yandex.ru>
2023-04-12 21:13:15 +0300

    Добавьте файлы проекта.

---
+++ CPP_Task3/CPP_Task3.cpp
@@ -0,0 +1,34 @@
+﻿#include "HashMap.h"
+#include <assert.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct MyKeyHash {
+    unsigned long operator()(const int& k) const
+    {
+        return k % 10;
+    }
+};
+
+int main()
+{
+    HashMap<int, string, 10, MyKeyHash> hmap;
+    hmap.put(1, "1");
+    hmap.put(2, "2");
+    hmap.put(3, "3");
+
+    string value;
+    bool result = hmap.get(2, value);
+    assert(result);
+    assert(value == "2");
+
+    result = hmap.get(3, value);
+    assert(result);
+    assert(value == "3");
+
+    hmap.remove(3);
+    result = hmap.get(3, value);
+    assert(!result);
+}
---
+++ CPP_Task3/KeyHash.h
@@ -0,0 +1,7 @@
+template <typename K, size_t tableSize>
+struct KeyHash {
+    unsigned long operator()(const K& key) const
+    {
+        return reinterpret_cast<unsigned long>(key) % tableSize;
+    }
+};
